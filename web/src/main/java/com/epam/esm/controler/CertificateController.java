7d2e9d16549624ef13c6d46cbe4bbecbe5964ac4
YanaV267 <yanochka.volkova.2002@gmail.com>
2022-04-01 21:36:12 +0300

    added web layer

---
+++ web/src/main/java/com/epam/esm/controler/CertificateController.java
@@ -0,0 +1,82 @@
+package com.epam.esm.controler;
+
+import com.epam.esm.dto.GiftCertificateDto;
+import com.epam.esm.exception.BadRequestException;
+import com.epam.esm.exception.NotFoundException;
+import com.epam.esm.service.GiftCertificateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/certificates")
+public class CertificateController {
+    private final GiftCertificateService certificateService;
+
+    @Autowired
+    public CertificateController(GiftCertificateService certificateService) {
+        this.certificateService = certificateService;
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    @ResponseStatus(CREATED)
+    public void create(@RequestBody Map<String, String> certificateData) {
+        boolean isCreated = certificateService.create(certificateData);
+        if (!isCreated) {
+            throw new BadRequestException();
+        }
+    }
+
+    @PutMapping(consumes = APPLICATION_JSON_VALUE)
+    @ResponseStatus(OK)
+    public void update(@RequestBody Map<String, String> certificateData) {
+        boolean isUpdated = certificateService.update(certificateData);
+        if (!isUpdated) {
+            throw new BadRequestException();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(OK)
+    public void delete(@PathVariable long id) {
+        boolean isDeleted = certificateService.delete(id);
+        if (!isDeleted) {
+            throw new NotFoundException();
+        }
+    }
+
+    @GetMapping
+    @ResponseStatus(FOUND)
+    public List<GiftCertificateDto> retrieveAll() {
+        return certificateService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(FOUND)
+    public GiftCertificateDto findById(@PathVariable long id) {
+        Optional<GiftCertificateDto> giftCertificate = certificateService.findById(id);
+        if (giftCertificate.isPresent()) {
+            return giftCertificate.get();
+        } else {
+            throw new NotFoundException();
+        }
+    }
+
+    @GetMapping
+    @ResponseStatus(FOUND)
+    public List<GiftCertificateDto> findByName(@RequestParam String name) {
+        return certificateService.findByName(name);
+    }
+
+    @GetMapping
+    @ResponseStatus(FOUND)
+    public List<GiftCertificateDto> findByDescription(@RequestParam String description) {
+        return certificateService.findByDescription(description);
+    }
+}
---
+++ web/src/main/java/com/epam/esm/controler/TagController.java
@@ -0,0 +1,60 @@
+package com.epam.esm.controler;
+
+import com.epam.esm.dto.TagDto;
+import com.epam.esm.exception.BadRequestException;
+import com.epam.esm.exception.NotFoundException;
+import com.epam.esm.service.TagService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/tags")
+public class TagController {
+    private final TagService tagService;
+
+    @Autowired
+    public TagController(TagService tagService) {
+        this.tagService = tagService;
+    }
+
+    @PostMapping(consumes = APPLICATION_JSON_VALUE)
+    @ResponseStatus(CREATED)
+    public void create(@RequestBody String tagName) {
+        boolean isCreated = tagService.create(tagName);
+        if (!isCreated) {
+            throw new BadRequestException();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(OK)
+    public void delete(@PathVariable long id) {
+        boolean isDeleted = tagService.delete(id);
+        if (!isDeleted) {
+            throw new NotFoundException();
+        }
+    }
+
+    @GetMapping
+    @ResponseStatus(FOUND)
+    public List<TagDto> retrieveAll() {
+        return tagService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(FOUND)
+    public TagDto findById(@PathVariable long id) {
+        Optional<TagDto> tag = tagService.findById(id);
+        if (tag.isPresent()) {
+            return tag.get();
+        } else {
+            throw new NotFoundException();
+        }
+    }
+}
